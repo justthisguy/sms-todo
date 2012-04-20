0078092c35e2d2fa9d456f796d8f5c5c551ea315
Ken Decanio <ken-decanio@41monkeys.com>
2012-04-19 22:12:02 -0700

    it works!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,6 +38,18 @@ class TasksController < ApplicationController
     @list = List.find(params[:list_id])
     @task = @list.tasks.find(params[:id])
 
+    account_sid = 'ACbbefae45e9c349aa931498bd315c85e1'
+    auth_token = '748a6fa66c8d70882fdbb9424b5c0944'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    if @task.phone
+      @client.account.sms.messages.create(
+        :from => '4155992671',
+        :to => @task.phone,
+        :body => @task.name + ' has been completed!'
+        )
+    end
+
     respond_to do |format|
       if @task.update_attributes(params[:task])
         format.html { redirect_to( list_tasks_url(@list), :notice => 'Task was successfully updated.') }
@@ -49,9 +61,8 @@ class TasksController < ApplicationController
 
 
   def destroy
+    
     @task = Task.find(params[:id])
-    
-    
     @task.destroy
 
     respond_to do |format|
